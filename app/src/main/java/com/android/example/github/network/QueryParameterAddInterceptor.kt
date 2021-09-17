865194d7dc775df8602b995b94157c417ec7f6db
Victoria Aloy <vic.aloy@gmail.com>
2021-09-17 18:11:27 -0300

    Nuevo commit

---
+++ app/src/main/java/com/android/example/github/network/QueryParameterAddInterceptor.kt
@@ -0,0 +1,22 @@
+package com.android.example.github.network
+
+import com.android.example.github.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class QueryParameterAddInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val url = chain.request().url().newBuilder()
+                .addQueryParameter("appid", BuildConfig.API_KEY)
+                .build()
+
+        val request = chain.request().newBuilder()
+                // .addHeader("Authorization", "Bearer token")
+                .url(url)
+                .build()
+
+        return chain.proceed(request)
+    }
+}
